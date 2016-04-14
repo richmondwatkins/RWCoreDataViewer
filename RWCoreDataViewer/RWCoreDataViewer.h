10896fde8715d970e61834ca88c88e6692e5d07d
Richmond Watkins <richmondwatkins@mac.com>
2016-04-14 18:22:28 -0500

    Initial Commit

---
+++ RWCoreDataViewer/RWCoreDataViewer.h
@@ -0,0 +1,19 @@
+//
+//  RWCoreDataViewer.h
+//  RWCoreDataViewer
+//
+//  Created by Richmond Watkins on 4/14/16.
+//  Copyright © 2016 Richmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWCoreDataViewer.
+FOUNDATION_EXPORT double RWCoreDataViewerVersionNumber;
+
+//! Project version string for RWCoreDataViewer.
+FOUNDATION_EXPORT const unsigned char RWCoreDataViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWCoreDataViewer/PublicHeader.h>
+
+
